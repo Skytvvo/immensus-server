51e59ed39d5427d96c11183d5c6b35b900630c49
ilyakazakou <ilyakazakou@gmail.com>
2024-04-03 01:49:57 +0300

    Created base api's

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
+import { AddressesModule } from './addresses/addresses.module';
+import { FiltersModule } from './filters/filters.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    OrdersModule,
+    ProductsModule,
+    CartModule,
+    AddressesModule,
+    FiltersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
