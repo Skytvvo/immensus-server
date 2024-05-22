ad32456d440d6abc930fccd4a7f1fb0e42566846
ilyakazakou <ilyakazakou@gmail.com>
2024-05-22 04:15:21 +0300

    - added user profile update api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Put, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AuthGuard } from '../auth/auth.guard';
+import { UpdateUserDto } from '../dto/user/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('all')
+  @UseGuards(AuthGuard)
+  async getAll(@Request() req) {
+    return await this.usersService.getAll(req.user.id);
+  }
+
+  @Put('update')
+  @UseGuards(AuthGuard)
+  async updateUser(@Body() updateUserDto: UpdateUserDto, @Request() req) {
+    return await this.usersService.updateUser(updateUserDto, req.user.id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from 'src/dto/user/create-user.dto';
-import { UserEntity } from 'src/entities/user.entity';
+import { UserEntity, UserRoles } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
+import { UpdateUserDto } from '../dto/user/update-user.dto';
+
 @Injectable()
 export class UsersService {
   constructor(
@@ -19,5 +21,24 @@ export class UsersService {
       updated: new Date(),
       carts: [],
     });
+  }
+
+  async getAll(id: string) {
+    const user = this.userRepository.findOneBy({ id, role: UserRoles.ADMIN });
+
+    if (!user) throw new ForbiddenException();
+
+    return await this.userRepository.find();
+  }
+
+  async updateUser(
+    { id: updatingUserId, ...updateUserDto }: UpdateUserDto,
+    id: string,
+  ) {
+    const updater = await this.userRepository.findOneBy({ id });
+    if (!updater || updater?.role !== UserRoles.ADMIN)
+      throw new ForbiddenException();
+
+    await this.userRepository.update(updatingUserId, updateUserDto);
   }
 }
